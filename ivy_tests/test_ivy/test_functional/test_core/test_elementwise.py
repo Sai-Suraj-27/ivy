91ea27b6e05eed22db022f967ff501c285b15e50
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-01-22 14:55:33 +0530

    Fixed ivy.not_equal for paddle backend

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -1564,7 +1564,15 @@ def test_negative(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
 @handle_test(
     fn_tree="functional.ivy.not_equal",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid", full=True), num_arrays=2
+        available_dtypes=(
+            ivy.complex128,
+            ivy.complex64,
+            ivy.float64,
+            ivy.float32,
+            ivy.int64,
+            ivy.int32,
+        ),
+        num_arrays=2,
     ),
     test_gradients=st.just(False),
 )
